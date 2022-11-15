2b4964ee40b104b6a61e5a9f0cd497c744a397c9
myjung <myjung.fro@hancom.com>
2022-11-15 15:07:23 +0900

    정민영 / 선임연 - 마스터

---
+++ master/diff_3_4_update.sql
@@ -0,0 +1,67 @@
+﻿use tcgprojectdb
+go
+
+begin transaction;
+
+
+-- tablename : avatarParts
+
+-- tablename : avatarPartsColorType
+
+-- tablename : avatarPartsGroupType
+
+-- tablename : avatarPartsSizeType
+
+-- tablename : avatarPartsStateType
+
+-- tablename : avatarPartsType
+
+-- tablename : mannequinModelType
+
+-- tablename : departmentType
+
+-- tablename : languageType
+
+-- tablename : osType
+
+-- tablename : providerType
+
+-- tablename : roleType
+
+-- tablename : npcMapPositionType
+
+-- tablename : jumpingMatchingGameType
+
+-- tablename : quizTimeType
+
+-- tablename : quizAnswerType
+
+-- tablename : countryCode
+
+-- tablename : faq
+
+-- tablename : forbiddenWords
+
+-- tablename : npcSeq
+
+-- tablename : npcSeqAct
+
+-- tablename : npc
+
+-- tablename : jumpingMatchingLevel
+
+-- tablename : quizLevel
+
+-- tablename : quizRoundTime
+
+-- tablename : quizQuestionAnswer
+
+-- tablename : avatarPartsPrice
+
+-- tablename : avatarPartsSizeDetail
+
+-- tablename : commerceZoneMannequin
+
+-- tablename : mannequinPurchaseState
+
+rollback;
---
+++ master/diff_4_3_delete.sql
@@ -0,0 +1,89 @@
+﻿use tcgprojectdb
+go
+
+begin transaction;
+
+
+-- tablename : avatarParts
+
+-- tablename : avatarPartsColorType
+
+-- tablename : avatarPartsGroupType
+
+-- tablename : avatarPartsSizeType
+
+-- tablename : avatarPartsStateType
+
+-- tablename : avatarPartsType
+
+-- tablename : mannequinModelType
+
+-- tablename : departmentType
+
+-- tablename : languageType
+
+-- tablename : memberType
+
+-- tablename : osType
+
+-- tablename : providerType
+
+-- tablename : roleType
+
+-- tablename : npcMapPositionType
+
+-- tablename : jumpingMatchingGameType
+
+-- tablename : quizTimeType
+
+-- tablename : quizAnswerType
+
+-- tablename : questConditionType
+
+-- tablename : questGroupType
+
+-- tablename : questMissionType
+
+-- tablename : questNameType
+
+-- tablename : questOpenType
+
+-- tablename : questRewardType
+
+-- tablename : questStepType
+
+-- tablename : questReward
+
+-- tablename : quest
+
+-- tablename : countryCode
+
+-- tablename : faq
+
+-- tablename : forbiddenWords
+
+-- tablename : npcSeq
+
+-- tablename : npcSeqAct
+
+-- tablename : npc
+
+-- tablename : jumpingMatchingLevel
+
+-- tablename : quizLevel
+
+-- tablename : quizRoundTime
+
+-- tablename : quizQuestionAnswer
+
+-- tablename : hallOfFame
+
+-- tablename : avatarPartsPrice
+
+-- tablename : avatarPartsSizeDetail
+
+-- tablename : commerceZoneMannequin
+
+-- tablename : mannequinPurchaseState
+
+rollback;
