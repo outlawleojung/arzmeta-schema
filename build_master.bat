09b31b6ea27b568c6cb5462ea8e3db239ea5bf4b
myjung <myjung.fro@hancom.com>
2022-12-19 20:56:43 +0900

    정민영 / 선임연 - 마스터 20

--- build_master.bat
+++ build_master.bat
@@ -10,6 +10,8 @@ set DESC_DIR=%OUTPUT_DIR%\%MASTER%
 
 set CP_FILE=master_schema.sql
 
+set CP_FILE=master.json
+
 copy .\%CP_FILE% %DESC_DIR%\%CP_FILE% /Y >nul
 
 set CP_FILE=sqlite_schema.sql
---
+++ master/diff_19_20_update.sql
@@ -0,0 +1,156 @@
+﻿use tcgprojectdb
+go
+
+begin transaction;
+
+
+-- tablename : avatarPartsColorType
+
+-- tablename : avatarPartsGroupType
+
+-- tablename : avatarPartsSizeType
+
+-- tablename : avatarPartsStateType
+
+-- tablename : avatarPartsType
+
+-- tablename : avatarPresetType
+
+-- tablename : mannequinModelType
+
+-- tablename : departmentType
+
+-- tablename : languageType
+
+-- tablename : osType
+
+-- tablename : providerType
+
+-- tablename : roleType
+
+-- tablename : npcMapPositionType
+
+-- tablename : jumpingMatchingGameType
+
+-- tablename : quizTimeType
+
+-- tablename : quizAnswerType
+
+-- tablename : voteDivType
+
+-- tablename : voteStateType
+
+-- tablename : voteAlterResType
+
+-- tablename : voteResType
+
+-- tablename : voteResultType
+
+-- tablename : worldtype
+
+-- tablename : areaType
+
+-- tablename : reportType
+
+-- tablename : categoryType
+
+-- tablename : gradeType
+
+-- tablename : itemType
+
+-- tablename : layerType
+
+-- tablename : packageType
+
+-- tablename : purchaseType
+
+-- tablename : saleType
+
+-- tablename : officeGradeType
+
+-- tablename : officeModeType
+
+-- tablename : officePermissionType
+
+-- tablename : officeSpawnType
+
+-- tablename : officeTopicType
+
+-- tablename : localization
+
+-- tablename : faq
+
+-- tablename : forbiddenWords
+
+-- tablename : avatarParts
+
+-- tablename : npcSeq
+
+-- tablename : npcSeqAct
+
+-- tablename : npc
+
+-- tablename : jumpingMatchingLevel
+
+-- tablename : quizLevel
+
+-- tablename : quizRoundTime
+
+-- tablename : quizQuestionAnswer
+
+-- tablename : avatarPartsPrice
+
+-- tablename : avatarPartsSizeDetail
+
+-- tablename : commerceZoneMannequin
+
+-- tablename : mannequinPurchaseState
+
+-- tablename : avatarPreset
+
+-- tablename : worldAreaInfo
+
+-- tablename : item
+
+-- tablename : interiorInstallInfo
+update interiorInstallInfo set itemId = 211001,prefab = N'My_Bed_01',layerType = 1,xSize = 2,ySize = 3,removable = 1 where itemId = 211001;
+update interiorInstallInfo set itemId = 212001,prefab = N'My_Chair_01',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 212001;
+update interiorInstallInfo set itemId = 213002,prefab = N'My_Drawer_02',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 213002;
+update interiorInstallInfo set itemId = 213003,prefab = N'My_Drawer_03',layerType = 1,xSize = 2,ySize = 2,removable = 1 where itemId = 213003;
+update interiorInstallInfo set itemId = 213004,prefab = N'My_Drawer_04',layerType = 1,xSize = 3,ySize = 3,removable = 1 where itemId = 213004;
+update interiorInstallInfo set itemId = 213005,prefab = N'My_Drawer_05',layerType = 1,xSize = 2,ySize = 2,removable = 1 where itemId = 213005;
+update interiorInstallInfo set itemId = 213006,prefab = N'My_Drawer_06',layerType = 1,xSize = 2,ySize = 2,removable = 1 where itemId = 213006;
+update interiorInstallInfo set itemId = 214001,prefab = N'My_Sofa_01',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 214001;
+update interiorInstallInfo set itemId = 215001,prefab = N'My_Table_01',layerType = 1,xSize = 2,ySize = 2,removable = 1 where itemId = 215001;
+update interiorInstallInfo set itemId = 215003,prefab = N'My_Table_03',layerType = 1,xSize = 2,ySize = 2,removable = 1 where itemId = 215003;
+update interiorInstallInfo set itemId = 215004,prefab = N'My_Table_04',layerType = 1,xSize = 2,ySize = 2,removable = 1 where itemId = 215004;
+update interiorInstallInfo set itemId = 215005,prefab = N'My_Table_05',layerType = 1,xSize = 2,ySize = 2,removable = 1 where itemId = 215005;
+update interiorInstallInfo set itemId = 240003,prefab = N'My_Carpet_03',layerType = 2,xSize = 3,ySize = 3,removable = 1 where itemId = 240003;
+update interiorInstallInfo set itemId = 221008,prefab = N'Christmas_Box_01',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 221008;
+update interiorInstallInfo set itemId = 221009,prefab = N'Christmas_Box_02',layerType = 1,xSize = 1,ySize = 2,removable = 1 where itemId = 221009;
+update interiorInstallInfo set itemId = 221010,prefab = N'Christmas_Box_03',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 221010;
+update interiorInstallInfo set itemId = 221011,prefab = N'Christmas_Box_04',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 221011;
+update interiorInstallInfo set itemId = 221012,prefab = N'Christmas_Santa_hat_01',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 221012;
+update interiorInstallInfo set itemId = 221013,prefab = N'Christmas_Snowman_01',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 221013;
+update interiorInstallInfo set itemId = 221014,prefab = N'Christmas_Tree_01',layerType = 1,xSize = 2,ySize = 2,removable = 1 where itemId = 221014;
+update interiorInstallInfo set itemId = 221015,prefab = N'Christmas_Wall_Decor_01',layerType = 3,xSize = 1,ySize = 1,removable = 1 where itemId = 221015;
+
+-- tablename : inventoryCapacity
+
+-- tablename : startInventory
+
+-- tablename : startMyRoom
+
+-- tablename : officeAuthority
+
+-- tablename : officeBookmark
+
+-- tablename : officeDefaultOption
+
+-- tablename : officeMode
+
+-- tablename : officeModeSlot
+
+-- tablename : officeRoomInfo
+
+rollback;
