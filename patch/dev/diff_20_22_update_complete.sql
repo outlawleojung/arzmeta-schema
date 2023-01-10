f8c84dc0319db4f113d1e8b95df3f2e2f8dfa27f
myjung <myjung.fro@hancom.com>
2023-01-10 09:55:04 +0900

    정민영 / 선임연 - 마스터 24

---
+++ patch/dev/diff_20_22_update_complete.sql
@@ -0,0 +1,137 @@
+﻿use dev_arzmeta_db;
+
+START TRANSACTION;
+
+
+-- tablename : avatarPartsColorType
+
+-- tablename : avatarPartsGroupType
+
+-- tablename : avatarPartsSizeType
+
+-- tablename : avatarPartsStateType
+
+-- tablename : avatarPartsType
+
+-- tablename : avatarPresetType
+
+-- tablename : mannequinModelType
+
+-- tablename : departmentType
+
+-- tablename : languageType
+
+-- tablename : osType
+
+-- tablename : providerType
+
+-- tablename : roleType
+
+-- tablename : npcMapPositionType
+
+-- tablename : jumpingMatchingGameType
+
+-- tablename : quizTimeType
+
+-- tablename : quizAnswerType
+
+-- tablename : voteDivType
+
+-- tablename : voteStateType
+
+-- tablename : voteAlterResType
+
+-- tablename : voteResType
+
+-- tablename : voteResultType
+
+-- tablename : worldtype
+
+-- tablename : areaType
+
+-- tablename : reportType
+
+-- tablename : cateryType
+
+-- tablename : gradeType
+
+-- tablename : itemType
+
+-- tablename : layerType
+
+-- tablename : packageType
+
+-- tablename : purchaseType
+
+-- tablename : saleType
+
+-- tablename : officeGradeType
+
+-- tablename : officeModeType
+
+-- tablename : officePermissionType
+
+-- tablename : officeSpawnType
+
+-- tablename : officeTopicType
+
+-- tablename : onfContentsType
+
+-- tablename : localization
+
+-- tablename : faq
+update faq set id = 13,question = N'궁금한 점이 있거나 문의사항이 있으면 어디로 연락해야 하나요?',answer = N'궁금한 사항 있으시면 help.arzmeta@hancom.com 로 문의주세요' where id = 13;
+
+-- tablename : forbiddenWords
+
+-- tablename : avatarParts
+
+-- tablename : npcSeq
+
+-- tablename : npcSeqAct
+
+-- tablename : npc
+
+-- tablename : jumpingMatchingLevel
+
+-- tablename : quizLevel
+
+-- tablename : quizRoundTime
+
+-- tablename : quizQuestionAnswer
+
+-- tablename : avatarPartsPrice
+
+-- tablename : avatarPartsSizeDetail
+
+-- tablename : commerceZoneMannequin
+
+-- tablename : mannequinPurchaseState
+
+-- tablename : avatarPreset
+
+-- tablename : worldAreaInfo
+
+-- tablename : item
+
+-- tablename : interiorInstallInfo
+
+-- tablename : inventoryCapacity
+
+-- tablename : startInventory
+
+-- tablename : startMyRoom
+
+-- tablename : officeAuthority
+
+-- tablename : officeBookmark
+
+-- tablename : officeDefaultOption
+
+-- tablename : officeMode
+
+-- tablename : officeModeSlot
+
+-- tablename : officeRoomInfo
+
+ROLLBACK;
