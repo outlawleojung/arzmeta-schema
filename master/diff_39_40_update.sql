e51e70454ab786ca117c635912339c4e0323ba90
myjung <myjung.fro@hancom.com>
2023-03-17 17:05:15 +0900

    정민영 / 선임연 - 마스터 40

---
+++ master/diff_39_40_update.sql
@@ -0,0 +1,178 @@
+﻿use dev_arzmeta_db;
+
+START TRANSACTION;
+
+
+-- tablename : localization
+
+-- tablename : avatarPartsColorType
+
+-- tablename : avatarPartsGroupType
+
+-- tablename : avatarPartsSizeType
+
+-- tablename : avatarPartsStateType
+
+-- tablename : avatarPartsType
+
+-- tablename : avatarPresetType
+
+-- tablename : mannequinModelType
+
+-- tablename : departmentType
+
+-- tablename : languageType
+
+-- tablename : osType
+
+-- tablename : providerType
+
+-- tablename : roleType
+
+-- tablename : npcMapPositionType
+
+-- tablename : jumpingMatchingGameType
+
+-- tablename : quizTimeType
+
+-- tablename : quizAnswerType
+
+-- tablename : voteDivType
+
+-- tablename : voteStateType
+
+-- tablename : voteAlterResType
+
+-- tablename : voteResType
+
+-- tablename : voteResultType
+
+-- tablename : worldtype
+
+-- tablename : areaType
+
+-- tablename : reportType
+
+-- tablename : categoryType
+
+-- tablename : gradeType
+
+-- tablename : itemType
+
+-- tablename : layerType
+
+-- tablename : packageType
+
+-- tablename : purchaseType
+
+-- tablename : saleType
+
+-- tablename : officeGradeType
+
+-- tablename : officeModeType
+
+-- tablename : officePermissionType
+
+-- tablename : officeSpawnType
+
+-- tablename : officeTopicType
+
+-- tablename : onfContentsType
+
+-- tablename : officeAlarmType
+
+-- tablename : officeExposureType
+
+-- tablename : paymentType
+
+-- tablename : inquiryType
+
+-- tablename : reportReasonType
+
+-- tablename : restrictionType
+
+-- tablename : restrictionDetail
+
+-- tablename : disciplineType
+
+-- tablename : disciplineDetail
+
+-- tablename : licenseType
+
+-- tablename : licenseFunction
+
+-- tablename : functionTable
+
+-- tablename : faq
+
+-- tablename : forbiddenWords
+
+-- tablename : item
+
+-- tablename : itemUseEffect
+
+-- tablename : avatarParts
+
+-- tablename : npcSeq
+
+-- tablename : npcSeqAct
+
+-- tablename : npc
+
+-- tablename : jumpingMatchingLevel
+
+-- tablename : quizLevel
+
+-- tablename : quizRoundTime
+
+-- tablename : quizQuestionAnswer
+
+-- tablename : commerceZoneMannequin
+
+-- tablename : commerceZoneItem
+
+-- tablename : mannequinPurchaseState
+
+-- tablename : avatarPreset
+
+-- tablename : worldAreaInfo
+
+-- tablename : interiorInstallInfo
+update interiorInstallInfo set itemId = 215007,prefab = N'-2146826246',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 215007;
+update interiorInstallInfo set itemId = 221016,prefab = N'-2146826246',layerType = 1,xSize = 1,ySize = 1,removable = 1 where itemId = 221016;
+
+-- tablename : startInventory
+insert into startInventory(itemId,count) values (215007,1);
+insert into startInventory(itemId,count) values (221016,1);
+
+-- tablename : startMyRoom
+
+-- tablename : officeAuthority
+
+-- tablename : officeBookmark
+
+-- tablename : officeDefaultOption
+
+-- tablename : officeMode
+
+-- tablename : officeModeSlot
+
+-- tablename : officeSpaceInfo
+
+-- tablename : officeExposure
+
+-- tablename : officeProductItem
+
+-- tablename : officeGradeAuthority
+
+-- tablename : businessCardTemplate
+
+-- tablename : avatarResetInfo
+
+-- tablename : reportCategory
+
+-- tablename : licenseTypeInfo
+
+-- tablename : disciplineReview
+
+ROLLBACK;
