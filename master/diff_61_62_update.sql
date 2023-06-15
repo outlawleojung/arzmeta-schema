2a408df83562d0d32020ab1b195ec06ace7395ea
outlawleojung <outlawleojung@gmail.com>
2023-06-15 13:17:15 +0900

    정민영 / 책임연 - master_62

---
+++ master/diff_61_62_update.sql
@@ -0,0 +1,204 @@
+﻿use dev_arzmeta_db;
+
+START TRANSACTION;
+
+
+-- tablename : localization
+update localization set id = N'map_arzhill_name',kor = N'베네Fit FOREST',eng = N'BeneFit Forest' where id = 'map_arzhill_name';
+update localization set id = N'map_arzhill_desc',kor = N'아즈메타의 작은 동산이에요. 지구를 위한 나무 심기, 아즈메타도 함께합니다.',eng = N'This is BeneFit Forest in a:rzLand. Planting trees for the earth, a:rzmeta is also with.' where id = 'map_arzhill_desc';
+
+-- tablename : avatarPartsColorType
+
+-- tablename : avatarPartsGroupType
+
+-- tablename : avatarPartsSizeType
+
+-- tablename : avatarPartsStateType
+
+-- tablename : avatarPresetType
+
+-- tablename : mannequinModelType
+
+-- tablename : languageType
+
+-- tablename : osType
+
+-- tablename : providerType
+
+-- tablename : roleType
+
+-- tablename : sceneType
+
+-- tablename : jumpingMatchingGameType
+
+-- tablename : quizTimeType
+
+-- tablename : quizAnswerType
+
+-- tablename : voteDivType
+
+-- tablename : voteStateType
+
+-- tablename : voteAlterResType
+
+-- tablename : voteResType
+
+-- tablename : voteResultType
+
+-- tablename : worldtype
+
+-- tablename : areaType
+
+-- tablename : reportType
+
+-- tablename : categoryType
+
+-- tablename : gradeType
+
+-- tablename : itemType
+
+-- tablename : layerType
+
+-- tablename : packageType
+
+-- tablename : paymentStateType
+
+-- tablename : officeGradeType
+
+-- tablename : officeModeType
+
+-- tablename : officePermissionType
+
+-- tablename : officeSpawnType
+
+-- tablename : officeTopicType
+
+-- tablename : onfContentsType
+
+-- tablename : officeAlarmType
+
+-- tablename : officeExposureType
+
+-- tablename : inquiryType
+
+-- tablename : reportReasonType
+
+-- tablename : restrictionType
+
+-- tablename : restrictionDetail
+
+-- tablename : disciplineType
+
+-- tablename : disciplineDetail
+
+-- tablename : licenseType
+
+-- tablename : licenseFunction
+
+-- tablename : landType
+
+-- tablename : mapInfoType
+
+-- tablename : myRoomStateType
+
+-- tablename : npcType
+
+-- tablename : npcLookType
+
+-- tablename : appendType
+
+-- tablename : postalLogType
+
+-- tablename : logActionType
+
+-- tablename : moneyType
+
+-- tablename : postalSendType
+
+-- tablename : dynamicLinkType
+
+-- tablename : functionTable
+
+-- tablename : faq
+
+-- tablename : forbiddenWords
+
+-- tablename : item
+
+-- tablename : itemUseEffect
+
+-- tablename : jumpingMatchingLevel
+
+-- tablename : quizLevel
+
+-- tablename : quizRoundTime
+
+-- tablename : quizQuestionAnswer
+
+-- tablename : commerceZoneMannequin
+
+-- tablename : commerceZoneItem
+
+-- tablename : mannequinPurchaseState
+
+-- tablename : avatarPreset
+
+-- tablename : worldAreaInfo
+
+-- tablename : interiorInstallInfo
+
+-- tablename : startInventory
+
+-- tablename : startMyRoom
+
+-- tablename : paymentProductManager
+
+-- tablename : officeAuthority
+
+-- tablename : officeBookmark
+
+-- tablename : officeDefaultOption
+
+-- tablename : officeMode
+
+-- tablename : officeModeSlot
+
+-- tablename : officeSpaceInfo
+
+-- tablename : officeExposure
+
+-- tablename : officeProductItem
+
+-- tablename : officeGradeAuthority
+
+-- tablename : officeShowRoomInfo
+
+-- tablename : officeSeatInfo
+
+-- tablename : businessCardTemplate
+
+-- tablename : avatarResetInfo
+
+-- tablename : reportCategory
+
+-- tablename : licenseTypeInfo
+
+-- tablename : disciplineReview
+
+-- tablename : mapExposulInfo
+
+-- tablename : mapExposulBrand
+
+-- tablename : npcList
+
+-- tablename : npcCostume
+
+-- tablename : npcArrange
+
+-- tablename : postalItemProperty
+
+-- tablename : postalMoneyProperty
+
+-- tablename : postalTypeProperty
+
+ROLLBACK;
---
+++ master/master_62/build.bat
@@ -0,0 +1,6 @@
+del *.db
+
+SQLiteE.exe master.db < sqlite_schema.sql
+SQLiteE.exe master.db < master_sqlite.sql
+
+pause
