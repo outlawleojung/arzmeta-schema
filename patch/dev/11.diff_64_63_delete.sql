d81224d4c4d772d717d2fe821a31aaa72b5f5881
outlawleojung <outlawleojung@gmail.com>
2023-06-19 19:57:32 +0900

    정민영 / 책임연 - master_64

---
+++ patch/dev/11.diff_64_63_delete.sql
@@ -0,0 +1,202 @@
+﻿use dev_arzmeta_db;
+
+START TRANSACTION;
+
+
+-- tablename : localization
+
+-- tablename : avatarPartsColorType
+
+-- tablename : avatarPartsGroupType
+
+-- tablename : avatarPartsSizeType
+
+-- tablename : avatarPartsStateType
+
+-- tablename : avatarPresetType
+
+-- tablename : mannequinModelType
+
+-- tablename : languageType
+
+-- tablename : osType
+
+-- tablename : providerType
+
+-- tablename : roleType
+
+-- tablename : sceneType
+
+-- tablename : jumpingMatchingGameType
+
+-- tablename : quizTimeType
+
+-- tablename : quizAnswerType
+
+-- tablename : voteDivType
+
+-- tablename : voteStateType
+
+-- tablename : voteAlterResType
+
+-- tablename : voteResType
+
+-- tablename : voteResultType
+
+-- tablename : worldtype
+
+-- tablename : areaType
+
+-- tablename : reportType
+
+-- tablename : categoryType
+
+-- tablename : gradeType
+
+-- tablename : itemType
+
+-- tablename : layerType
+
+-- tablename : packageType
+
+-- tablename : paymentStateType
+
+-- tablename : officeGradeType
+
+-- tablename : officeModeType
+
+-- tablename : officePermissionType
+
+-- tablename : officeSpawnType
+
+-- tablename : officeTopicType
+
+-- tablename : onfContentsType
+
+-- tablename : officeAlarmType
+
+-- tablename : officeExposureType
+
+-- tablename : inquiryType
+
+-- tablename : reportReasonType
+
+-- tablename : restrictionType
+
+-- tablename : restrictionDetail
+
+-- tablename : disciplineType
+
+-- tablename : disciplineDetail
+
+-- tablename : licenseType
+
+-- tablename : licenseFunction
+
+-- tablename : landType
+
+-- tablename : mapInfoType
+
+-- tablename : myRoomStateType
+
+-- tablename : npcType
+
+-- tablename : npcLookType
+
+-- tablename : appendType
+
+-- tablename : postalLogType
+
+-- tablename : logActionType
+
+-- tablename : moneyType
+
+-- tablename : postalSendType
+
+-- tablename : dynamicLinkType
+
+-- tablename : functionTable
+
+-- tablename : faq
+
+-- tablename : forbiddenWords
+
+-- tablename : item
+
+-- tablename : itemUseEffect
+
+-- tablename : jumpingMatchingLevel
+
+-- tablename : quizLevel
+
+-- tablename : quizRoundTime
+
+-- tablename : quizQuestionAnswer
+
+-- tablename : commerceZoneMannequin
+
+-- tablename : commerceZoneItem
+
+-- tablename : mannequinPurchaseState
+
+-- tablename : avatarPreset
+
+-- tablename : worldAreaInfo
+
+-- tablename : interiorInstallInfo
+
+-- tablename : startInventory
+
+-- tablename : startMyRoom
+
+-- tablename : paymentProductManager
+
+-- tablename : officeAuthority
+
+-- tablename : officeBookmark
+
+-- tablename : officeDefaultOption
+
+-- tablename : officeMode
+
+-- tablename : officeModeSlot
+
+-- tablename : officeSpaceInfo
+
+-- tablename : officeExposure
+
+-- tablename : officeProductItem
+
+-- tablename : officeGradeAuthority
+
+-- tablename : officeShowRoomInfo
+
+-- tablename : officeSeatInfo
+
+-- tablename : businessCardTemplate
+
+-- tablename : avatarResetInfo
+
+-- tablename : reportCategory
+
+-- tablename : licenseTypeInfo
+
+-- tablename : disciplineReview
+
+-- tablename : mapExposulInfo
+
+-- tablename : mapExposulBrand
+
+-- tablename : npcList
+
+-- tablename : npcCostume
+
+-- tablename : npcArrange
+
+-- tablename : postalItemProperty
+
+-- tablename : postalMoneyProperty
+
+-- tablename : postalTypeProperty
+
+ROLLBACK;
