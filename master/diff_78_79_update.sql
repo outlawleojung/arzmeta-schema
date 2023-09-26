0ecf6220f08bc604f8cfccad766dc398a20cf6e4
outlawleojung <outlawleojung@gmail.com>
2023-09-27 08:55:17 +0900

    정민영 / 책임연 - master_79

---
+++ master/diff_78_79_update.sql
@@ -0,0 +1,256 @@
+﻿use dev_arzmeta_db;
+
+START TRANSACTION;
+
+
+-- tablename : localization
+
+-- tablename : avatarPartsColorType
+
+-- tablename : avatarPartsGroupType
+
+-- tablename : avatarPartsSizeType
+
+-- tablename : avatarPartsStateType
+
+-- tablename : avatarPresetType
+
+-- tablename : mannequinModelType
+
+-- tablename : languageType
+
+-- tablename : osType
+
+-- tablename : providerType
+
+-- tablename : roleType
+
+-- tablename : sceneType
+
+-- tablename : jumpingMatchingGameType
+
+-- tablename : quizTimeType
+
+-- tablename : quizAnswerType
+
+-- tablename : voteDivType
+
+-- tablename : voteStateType
+
+-- tablename : voteAlterResType
+
+-- tablename : voteResType
+
+-- tablename : voteResultType
+
+-- tablename : voteResultExposureType
+
+-- tablename : worldtype
+
+-- tablename : areaType
+
+-- tablename : reportType
+
+-- tablename : categoryType
+
+-- tablename : gradeType
+
+-- tablename : itemType
+
+-- tablename : layerType
+
+-- tablename : packageType
+
+-- tablename : paymentStateType
+
+-- tablename : mediaRollingType
+
+-- tablename : officeGradeType
+
+-- tablename : officeModeType
+
+-- tablename : officePermissionType
+
+-- tablename : officeSpawnType
+
+-- tablename : officeTopicType
+
+-- tablename : onfContentsType
+
+-- tablename : officeAlarmType
+
+-- tablename : officeExposureType
+
+-- tablename : inquiryType
+
+-- tablename : reportReasonType
+
+-- tablename : restrictionType
+
+-- tablename : restrictionDetail
+
+-- tablename : disciplineType
+
+-- tablename : disciplineDetail
+
+-- tablename : licenseType
+
+-- tablename : uploadType
+
+-- tablename : spaceType
+
+-- tablename : spaceDetailType
+
+-- tablename : screenContentType
+
+-- tablename : licenseFunction
+
+-- tablename : landType
+
+-- tablename : mapInfoType
+
+-- tablename : myRoomStateType
+
+-- tablename : npcType
+
+-- tablename : npcLookType
+
+-- tablename : appendType
+
+-- tablename : postalLogType
+
+-- tablename : logActionType
+
+-- tablename : moneyType
+
+-- tablename : postalSendType
+
+-- tablename : dynamicLinkType
+
+-- tablename : eventSpaceType
+
+-- tablename : objectInteractionType
+
+-- tablename : selectVoteStateType
+
+-- tablename : bannerType
+
+-- tablename : ktmfPassTierRatingType
+
+-- tablename : fileBoxType
+
+-- tablename : adContents
+
+-- tablename : functionTable
+
+-- tablename : screenInfo
+
+-- tablename : bannerInfo
+
+-- tablename : spaceInfo
+
+-- tablename : faq
+
+-- tablename : forbiddenWords
+
+-- tablename : item
+
+-- tablename : itemMaterial
+update itemMaterial set itemId = 460001,num = 1,material = N'c_m_ktmfspecialsunglasses_c_trp_001' where itemId = 460001 and num = 1;
+update itemMaterial set itemId = 460001,num = 2,material = N'c_m_ktmfspecialsunglasses_c_001' where itemId = 460001 and num = 2;
+update itemMaterial set itemId = 460002,num = 1,material = N'c_m_ktmfspecialsunglasses_c_trp_002' where itemId = 460002 and num = 1;
+update itemMaterial set itemId = 460002,num = 2,material = N'c_m_ktmfspecialsunglasses_c_002' where itemId = 460002 and num = 2;
+update itemMaterial set itemId = 460003,num = 1,material = N'c_m_ktmfspecialsunglasses_c_trp_003' where itemId = 460003 and num = 1;
+update itemMaterial set itemId = 460003,num = 2,material = N'c_m_ktmfspecialsunglasses_c_003' where itemId = 460003 and num = 2;
+update itemMaterial set itemId = 460004,num = 1,material = N'c_m_ktmfspecialsunglasses_c_trp_004' where itemId = 460004 and num = 1;
+update itemMaterial set itemId = 460004,num = 2,material = N'c_m_ktmfspecialsunglasses_c_004' where itemId = 460004 and num = 2;
+update itemMaterial set itemId = 460005,num = 1,material = N'c_m_ktmfspecialsunglasses_c_trp_005' where itemId = 460005 and num = 1;
+update itemMaterial set itemId = 460005,num = 2,material = N'c_m_ktmfspecialsunglasses_c_005' where itemId = 460005 and num = 2;
+update itemMaterial set itemId = 460006,num = 1,material = N'c_m_ktmfspecialsunglasses_c_trp_006' where itemId = 460006 and num = 1;
+update itemMaterial set itemId = 460006,num = 2,material = N'c_m_ktmfspecialsunglasses_c_006' where itemId = 460006 and num = 2;
+update itemMaterial set itemId = 460007,num = 1,material = N'c_m_ktmfspecialsunglasses_c_trp_007' where itemId = 460007 and num = 1;
+update itemMaterial set itemId = 460007,num = 2,material = N'c_m_ktmfspecialsunglasses_c_007' where itemId = 460007 and num = 2;
+
+-- tablename : itemUseEffect
+
+-- tablename : jumpingMatchingLevel
+
+-- tablename : quizLevel
+
+-- tablename : quizRoundTime
+
+-- tablename : quizQuestionAnswer
+
+-- tablename : commerceZoneMannequin
+
+-- tablename : commerceZoneItem
+
+-- tablename : mannequinPurchaseState
+
+-- tablename : avatarPreset
+
+-- tablename : worldAreaInfo
+
+-- tablename : interiorInstallInfo
+
+-- tablename : startInventory
+
+-- tablename : startMyRoom
+
+-- tablename : paymentProductManager
+
+-- tablename : officeAuthority
+
+-- tablename : officeBookmark
+
+-- tablename : officeDefaultOption
+
+-- tablename : officeMode
+
+-- tablename : officeModeSlot
+
+-- tablename : officeSpaceInfo
+
+-- tablename : officeExposure
+
+-- tablename : officeProductItem
+
+-- tablename : officeGradeAuthority
+
+-- tablename : officeShowRoomInfo
+
+-- tablename : officeSeatInfo
+
+-- tablename : businessCardTemplate
+
+-- tablename : avatarResetInfo
+
+-- tablename : reportCategory
+
+-- tablename : licenseTypeInfo
+
+-- tablename : disciplineReview
+
+-- tablename : mapExposulInfo
+
+-- tablename : mapExposulBrand
+
+-- tablename : npcList
+
+-- tablename : npcCostume
+
+-- tablename : npcArrange
+
+-- tablename : postalItemProperty
+
+-- tablename : postalMoneyProperty
+
+-- tablename : postalTypeProperty
+
+-- tablename : ktmfspecialitem
+
+-- tablename : ktmfnfttoken
+
+-- tablename : ktmfspecialmoney
+
+ROLLBACK;
